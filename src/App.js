1caec65aa6598ed914fd6c4931a81d3965a4bf24
SchiavonNathan <nathanmestrepk@gmail.com>
2024-11-04 10:50:30 -0300

    Created page Cadastro and navigate function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import Login from './Login';
+import Cadastro from './Cadastro';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Router>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/Cadastro.js" element={<Cadastro />} />
+    </Routes>
+  </Router>
   );
 }
 
---
+++ src/Login.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.get('http://localhost:3001/users', {
+        name,
+        password,
+      });
+      alert(`Login bem-sucedido! Token: ${response.data.access_token}`);
+      // Aqui você pode armazenar o token em localStorage ou contexto
+    } catch (error) {
+      setError('Username ou senha incorretos.');
+    }
+  };
+
+  return (
+    <div style={{ maxWidth: '400px', margin: 'auto', padding: '20px' }}>
+      <h2>Página de Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Username:</label>
+          <input
+            type="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Senha:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+        <button type="submit">Entrar</button>
+      </form>
+      <Link to="./Cadastro.js">
+          <button>Cadastrar</button>
+      </Link>
+     </div>
+  );
+};
+
+export default Login;
