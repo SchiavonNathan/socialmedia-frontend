0febbf184f4f2f76806b531c61391b4f1b3de16d
SchiavonNathan <Nathanmestrepk@gmail.com>
2024-11-25 16:07:51 -0300

    Atualizadas funcoes de postagem (like, compartilhar, comentar)

--- src/pages/Postagem.js
+++ src/pages/Postagem.js
@@ -227,15 +227,36 @@ const Postagem = () => {
                       />
                     )}
                     {/* Botão like/deslike */}
+                    
+                    <div className='d-flex align-items-left justify-content-left gap-1'>
                     <small>{likesCount}</small>
-                <Button variant="link" onClick={handleLike}>
-                  {liked ? 'Descurtir' : 'Curtir'}
-                </Button>
-                    <ShareButton
-                      url={`${window.location.origin}/postagem/${postagem.id}`}
-                      title={postagem.titulo}
-                    />
-                    <button as="button" class="btn btn-primary" onClick={() => abrirModalParaEdicaoComentario(comentario)}>Comentar💬</button>
+                    <button class="btn btn-outline-danger"  onClick={handleLike}>
+
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16"><path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15"/></svg>
+                      {liked ? ' Descurtir' : ' Curtir'}
+                    </button>
+                    
+                    <button as="button" class="btn btn-outline-primary" onClick={() => abrirModalParaEdicaoComentario(comentario)}> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-left" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4.414A2 2 0 0 0 3 11.586l-2 2V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/></svg> Comentar</button>
+                    
+                    <button 
+                        onClick={() => {
+                          const shareData = {
+                            url: `${window.location.origin}/postagem/${postagem.id}`,
+                            title: postagem.titulo,
+                          };
+                      
+                          if (navigator.share) {
+                            navigator
+                              .share(shareData)
+                              .then(() => console.log('Compartilhamento realizado com sucesso!'))
+                              .catch((error) => console.error('Erro ao compartilhar:', error));
+                          }
+                        }}
+                        className="btn btn-outline-success"
+                    ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-share" viewBox="0 0 16 16">
+                    <path d="M13.5 1a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.5 2.5 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5m-8.5 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3m11 5.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3"/></svg> Compartilhar</button>
+                    </div>
+
                     <Dropdown className='drop' onClick={(e) => e.stopPropagation()}>
                         <Dropdown.Toggle variant="btn btn-primary" id="dropdown-custom-components" >
                           <FaBars />
@@ -252,6 +273,9 @@ const Postagem = () => {
                           <Dropdown.Item as="button" onClick={() => handleCopyLink(postagem.id)}>Copiar Link</Dropdown.Item>
                         </Dropdown.Menu>
                       </Dropdown>
+                      
+
+
                   </Card.Body>
                 </div>
               </Col>
